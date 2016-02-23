f6222e011185ef2901775bc898665a7f25b2cb29
nheyn <nick@heyn.io>
2016-02-23 15:40:51 -0700

    Add Documentation for the Request's 'encode(...)'/'decode(...)' Functions
    Add in-code documentation for the request's 'encode(...)' and 'decode(...)' functions.

--- src/dispatchRequest.js
+++ src/dispatchRequest.js
@@ -2,11 +2,21 @@
  * @flow
  */
 
-type ReqData = Object; //TODO
+type ReqData = {
+	startingPoints: {[key: string]: { state: string, index: number }};
+	actions: Array<Object>
+};
 type DecodedPause = { startingPoints: StartingPoints, actions: Array<Action> };
 
 /**
- * //TODO
+ * Encodes the paused state of the dispatcher into json to be passed over an HTTP request.
+ *
+ * @oaram pausePoints	{StartingPoints}			The state and place the dispatcher was paused
+ * @param actions		{Array<Actions>}			The actions to perform on the server
+ * @param encodeState 	{(string, any) => string}	A function that will encode the state, of a given store, to be
+ *													passed over an HTTP request
+ *
+ * @return				{ReqData}					The data to pass in the HTTP request
  */
 export function encode(pausePoints: StartingPoints, actions: Array<Action>, encodeState: EncodeStateFunc): ReqData {
 	//TODO
@@ -14,7 +24,12 @@ export function encode(pausePoints: StartingPoints, actions: Array<Action>, enco
 }
 
 /**
- * //TODO
+ * Decodes the data into the starting points and actions to perform on the server.
+ *
+ * @param data			{ReqData}					The data from the client
+ * @param decodeState	{(string, string) => any}	A function that will decode the state of a given store
+ *
+ * @return				{DecodePause}				The starting points and actions to start the dispatch from
  */
 export function decode(data: ReqData, decodeState: DecodeStateFunc): DecodedPause {
 	//TODO
