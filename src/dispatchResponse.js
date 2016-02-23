d0bbf2c5b752dbd269c66aa40294d9924e7385fc
nheyn <nick@heyn.io>
2016-02-23 15:45:57 -0700

    Add Documentation for the Response's 'encode(...)'/'decode(...)' Functions
    Add in-code documentation for the response's 'encode(...)' and 'decode(...)' functions.

--- src/dispatchResponse.js
+++ src/dispatchResponse.js
@@ -2,11 +2,18 @@
  * @flow
  */
 
-
-type ResData = Object; //TODO
+type ResData = {
+	newStates: {[key: string]: string}
+};
 
 /**
- * //TODO
+ * Encode the updated states, to send to the client.
+ *
+ * @param updatedStates	{StatesObject}				The updated states
+ * @param encodeState	{(string, any) => string}	A function that will encode the state, of a given store, to be
+ *													passed over an HTTP request
+ *
+ * @return				{ResData}					The data to respond to the client with
  */
 export function encode(updatedStates: StatesObject, encodeState: EncodeStateFunc): ResData {
 	//TODO
@@ -14,7 +21,12 @@ export function encode(updatedStates: StatesObject, encodeState: EncodeStateFunc
 }
 
 /**
- * //TODO
+ * Decode the states from the server.
+ *
+ * @param response		{ResData}					The response form the server
+ * @param decodeState	{(string, string) => any}	A function that will decode the state of a given store
+ *
+ * @return				{StatesObject}				The updated states
  */
 export function decode(response: ResData, decodeState: DecodeStateFunc): StatesObject {
 	//TODO
