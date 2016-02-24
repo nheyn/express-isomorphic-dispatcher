c514e8d7d4d4f9db401de39c9fd975b9f0122fbc
nheyn <nick@heyn.io>
2016-02-23 21:52:04 -0700

    Add Basic Info Store
    Create a 'basicInfo' isomorphic-dispatcher Store.

--- example/src/stores.js
+++ example/src/stores.js
@@ -1,3 +1,32 @@
 import { createStore } from 'isomorphic-dispatcher';
 
-export default {};
+// Basic Info Store
+export const BASIC_INFO_SET_TITLE = 'BASIC_INFO_SET_TITLE';
+export const BASIC_INFO_ADD_AUTHOR = 'BASIC_INFO_ADD_AUTHOR';
+const basicInfoInitialState = {
+	title: 'Untitled',
+	authors: []
+};
+
+const basicInfo = createStore(basicInfoInitialState).register((state, action) => {
+	if(action.type !== BASIC_INFO_SET_TITLE) return state;
+	if(typeof action.title !== 'string') {
+		console.error('The title for BASIC_INFO_SET_TITLE must be a string');
+		return state;
+	}
+
+	return { ...state, title: action.title}
+}).register((state, action) => {
+	if(action.type !== BASIC_INFO_ADD_AUTHOR) return state;
+	if(typeof action.author !== 'string') {
+		console.error('The author for BASIC_INFO_ADD_AUTHOR must be a string');
+		return state;
+	}
+
+	const authors = Array.from(state.authors);
+	authors.push(action.author);
+
+	return { ...state, authors };
+});
+
+export default { basicInfo };
