da85582462e257e667ada762ab99d6d727ede80e
nheyn <nick@heyn.io>
2016-02-23 21:17:22 -0700

    Render 'Hello World'
    Add basic App component, currently just renders "Hello World".

---
+++ example/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * React Component that render the entire app.
+ */
+const App = React.createClass({
+	render() {
+		return <div>Hello World</div>;
+	}
+});
+export default App;
